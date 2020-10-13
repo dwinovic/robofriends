a96ee96e79fe4eec6a55e6c4acdbff654f641ead
dwinovic <nd.cahya19@gmail.com>
2020-10-13 22:31:29 +0700

    create users with Fetch API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import { robots } from "./robots";
+import CardList from "./CardList"; 
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   } 
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}))
+  }
 
   onSearchChange = (e) => { 
     this.setState({searchfield: e.target.value}) 
