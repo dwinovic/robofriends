f4586604e62cb04a95ee9fa7cc78a337feb36093
dwinovic <nd.cahya19@gmail.com>
2020-10-13 17:37:26 +0700

    Changing heading

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     
     return (
       <div className="tc">
-        <h1> Robofriends </h1> 
+        <h1 className="f1"> Robofriends </h1> 
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
