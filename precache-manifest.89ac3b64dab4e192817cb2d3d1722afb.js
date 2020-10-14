67d048f629c7ea9929cc7465eea3ebe1c39964fc
dwinovic <nd.cahya19@gmail.com>
2020-10-14 21:05:00 +0700

    Updates

---
+++ precache-manifest.89ac3b64dab4e192817cb2d3d1722afb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea04ae7964ff7bafd9ed09ac6ce64f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7953fcfb5e1ba179fffb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f8f89676b45938261b89",
+    "url": "/static/css/main.6ed0b258.chunk.css"
+  },
+  {
+    "revision": "7953fcfb5e1ba179fffb",
+    "url": "/static/js/2.3524db9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3524db9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f89676b45938261b89",
+    "url": "/static/js/main.4025d787.chunk.js"
+  },
+  {
+    "revision": "98faa1f90ba787d3562e",
+    "url": "/static/js/runtime-main.203a6646.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
